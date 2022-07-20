4539cb4f5d8d64108a8be9704f9047767f0ae2e4
shahery <shaheryar924@gmail.com>
2022-07-20 10:42:00 +0000

    Start product template

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
 from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.index, name='home')
 ]
